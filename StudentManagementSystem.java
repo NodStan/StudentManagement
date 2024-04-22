a00f433294215b1fcb45a8b5003bae4a7ec89655
Stanley <stanleyobitunwase@gmail.com>
2024-04-22 14:28:33 +0100

    first commit

---
+++ StudentManagementSystem.java
@@ -0,0 +1,19 @@
+package com.example.studentmanagement;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StudentManagementSystem extends Application {
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
