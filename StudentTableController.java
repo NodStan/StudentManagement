8cc119171f88b8ac19e7a5b53c0f674c085c1271
Stanley <stanleyobitunwase@gmail.com>
2024-04-24 13:30:17 +0100

    first

--- StudentTableController.java
+++ StudentTableController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.sql.SQLException;
@@ -14,19 +15,33 @@ import java.util.ResourceBundle;
 public class StudentTableController implements Initializable {
     @FXML
     TableView<StudentModel> studentsTable;
+    ObservableList<StudentModel> studentsList;
     @Override
     public void initialize(URL location, ResourceBundle resources){
-        ObservableList<StudentModel> studentsList;
         try {
             StudentDao dao =new StudentDao();
-            studentsList = (ObservableList<StudentModel>) dao.getAllStudent();
+            studentsList = FXCollections.observableList(dao.getAllStudent());
 
-        } catch (ClassNotFoundException e) {
-            throw  new RuntimeException(e);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+// Creation of table columns
         TableColumn<StudentModel, String> firstNameColumn = new TableColumn<>();
+        TableColumn<StudentModel, String> lastNameColumn = new TableColumn<>();
+        TableColumn<StudentModel, String> emailColumn = new TableColumn<>();
+        TableColumn<StudentModel, String> courseColumn = new TableColumn<>();
+        TableColumn<StudentModel, Integer> ageColumn = new TableColumn<>();
 //        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+
+// map our details from the list to the columns
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        courseColumn.setCellValueFactory(new PropertyValueFactory<>("course"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<>("age"));
+
+// add all the columns to the table
+        studentsTable.getColumns().addAll(firstNameColumn, lastNameColumn, emailColumn, courseColumn, ageColumn);
     }
 }
